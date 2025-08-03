c3cef541ce95427d637e66c0b5efa60df71e1006
Niranjan Biju <138515670+Niranjan-Biju@users.noreply.github.com>
2025-08-03 23:53:58 +0530

    Update palindrome_number.py

--- problems-easy/palindrome_number.py
+++ problems-easy/palindrome_number.py
@@ -16,3 +16,19 @@ class Solution:
             num//=10
         
         return sum_val==x
+
+# Optimised
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # Negative numbers and numbers ending in 0 (but not 0 itself) can't be palindromes
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+
+        # If the number is even-length, x should equal reversed_half
+        # If the number is odd-length, x should equal reversed_half // 10 (middle digit doesn't matter)
+        return x == reversed_half or x == reversed_half // 10
